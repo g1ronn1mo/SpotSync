f81443fb17416644a2e1279f1e54533e446c4fba
g1ronn1mo <85943218+g1ronn1mo@users.noreply.github.com>
2022-06-25 13:04:17 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# first, ask for all needed information
+
+SYNC_FOLDER = "" # folder you want to download and sync your music
+USER = "" # your spotify user name
+CLIENT_ID = "" 
+CLIENT_SECRET = ""
+REDIRECT_URI = ""
